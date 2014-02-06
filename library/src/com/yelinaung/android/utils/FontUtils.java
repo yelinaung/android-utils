af45a2935b188d2ea54f6c231a9fcc5bb57624d6
Ye Lin Aung <me@yelinaung.com>
2014-02-06 11:55:45 +0630

    Added Myanmar Font detection by Indexer.

---
+++ library/src/com/yelinaung/android/utils/FontUtils.java
@@ -0,0 +1,16 @@
+package com.yelinaung.android.utils;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+/**
+ * Created by Ye Lin Aung on 14/02/06.
+ */
+public class FontUtils {
+
+    public boolean checkMMFont(String s) {
+        Pattern p = Pattern.compile("[^\\u0000-\\u0080]+");
+        Matcher matcher6 = p.matcher(s);
+        return matcher6.find();
+    }
+}
